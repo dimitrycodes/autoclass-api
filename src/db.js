36ce36f98f300602a55b19c80ae554f51006a506
Dimitry A <strongsafetyking@yahoo.com>
2021-03-05 12:21:50 -0500

    heroku5

--- src/db.js
+++ src/db.js
@@ -1,6 +1,10 @@
 const Pool = require('pg').Pool;
 const config = require('./config');
 
+const pg = require('pg'); 
+
+pg.defaults.ssl = process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false;
+
 const pool = new Pool({
   // user: 'postgres',
   // password: 'mypostgres',
