1371e1b148fc9f36eb5a4436de0c47a72b645c72
Dimitry A <strongsafetyking@yahoo.com>
2021-03-05 12:08:01 -0500

    heroku3

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool;
+const config = require('./config');
 
 const pool = new Pool({
-  user: 'postgres',
-  password: 'mypostgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'goatsapp'
+  // user: 'postgres',
+  // password: 'mypostgres',
+  // host: 'localhost',
+  // port: 5432,
+  // database: 'goatsapp'
+  connectionString: config.DATABASE_URL,
 }); 
 
 module.exports = pool;
