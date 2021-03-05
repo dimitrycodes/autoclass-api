0d06f7300ac25204d74e203bb7c00b27daea2044
Dimitry A <strongsafetyking@yahoo.com>
2021-03-05 11:36:25 -0500

    heroku2

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const { PORT } = require("./config");
 
 app.listen(PORT, () => {
-  console.log(`Express server is listening at http://localhost:${PORT}`);
+  //console.log(`Express server is listening at http://localhost:${PORT}`);
 });
